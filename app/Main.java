af1620b903f5d89df240b2e0c40b225b19428ee0
vampwave <vakarenkonikita82@gmail.com>
2025-06-25 12:33:55 +0300

    Add app description

--- app/Main.java
+++ app/Main.java
@@ -1,21 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My app for Users!");
-
+        System.out.println("Super app!");
+        System.out.println("Calculator for base calculations");
         int a = 8;
         int b = 2;
         int c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
-        System.out.println(d);
     }
 
     private static int add(int a, int b) {
         return a + b;
-    }
-
-    private static int multiply(int a, int b) {
-        return a * b;
     }
 }
